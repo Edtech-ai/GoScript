3821b07ba9d5d5a93120b01a2028c108f0529373
Edtech-ai <edtech.ai10@gmail.com>
2022-10-08 08:51:22 +0330

    functioncall2

--- hello.go
+++ hello.go
@@ -5,10 +5,21 @@ import "fmt"
 func main() {
 
 	hello()
+	addnumbers(3, 5, 7)
 
 }
 
 func hello() {
 
 	fmt.Println("Hello World!")
+}
+
+func addnumbers(x, y, z int) {
+
+	a := x + y
+	b := y + z
+	c := x + z
+
+	fmt.Println("The results are:", a, b, c)
+
 }
