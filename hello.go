f1af77f39dde732b7a74887bdc37acfdce8080ed
Edtech-ai <edtech.ai10@gmail.com>
2022-10-18 17:12:41 +0330

    Session5

--- hello.go
+++ hello.go
@@ -4,30 +4,54 @@ import "fmt"
 
 func main() {
 
-	hello()
-	addnumbers(3, 5, 7)
-	result := double(10)
-	fmt.Println(result)
+	// hello()
+	// addnumbers(3, 5, 7)
+	// result := double(10)
+	// fmt.Println(result)
+
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1=", 1+1)
+
+	fmt.Println(7.0 / 8)
+
+	fmt.Println(true && false)
+	fmt.Println(!true || !false)
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 10, 20
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
 
 }
 
-func hello() {
+// func hello() {
 
-	fmt.Println("Hello World!")
-}
+// 	fmt.Println("Hello World!")
+// }
 
-func addnumbers(x, y, z int) {
+// func addnumbers(x, y, z int) {
 
-	a := x + y
-	b := y + z
-	c := x + z
+// 	a := x + y
+// 	b := y + z
+// 	c := x + z
 
-	fmt.Println("The results are:", a, b, c)
+// 	fmt.Println("The results are:", a, b, c)
 
-}
+// }
 
-func double(x int) int {
+// func double(x int) int {
 
-	y := x * 2
-	return y
-}
+// 	y := x * 2
+// 	return y
+// }
